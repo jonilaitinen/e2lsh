2b864b2c48c967a70a2c2a1c8bc59c74dbebeed5
Joni Laitinen <jonilaitinen@astri.org>
2017-04-19 18:02:05 +0800

    Added interface to give the data as two dimensional double vector

--- sources/LSH.cpp
+++ sources/LSH.cpp
@@ -7,10 +7,45 @@ LSH::LSH() {
 
 }
 
+void LSH::computeParametersForData(const std::vector<std::vector<double> >& data) {
+
+	if(data.empty()) {
+		printf("Empty data vector!");
+		return;
+	}
+
+	IntT nPoints = data.size();
+	IntT pointsDimension = data[0].size();
+	RealT successProbability = 0.9;
+	RealT thresholdR = 0.3; // 0.005 0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.31 0.32 0.33 0.34 0.35 0.36 0.38 0.4
+	MemVarT availableTotalMemory = 4000000; // ~4GB
+
+	PPointT* dataSet = NULL;
+	FAILIF(NULL == (dataSet = (PPointT*)MALLOC(nPoints * sizeof(PPointT))));
+
+	for(IntT i = 0; i < data.size(); i++) {
+		PPointT p;
+		RealT sqrLength = 0;
+		FAILIF(NULL == (p = (PPointT)MALLOC(sizeof(PointT))));
+		FAILIF(NULL == (p->coordinates = (RealT*)MALLOC(pointsDimension * sizeof(RealT))));
+
+		for(IntT d = 0; d < pointsDimension; d++){
+			p->coordinates[d] = data[i][d];
+			sqrLength += SQR(p->coordinates[d]);
+		}
+		p->index = i;
+		p->sqrLength = sqrLength;
+		dataSet[i] = p;
+	}
+
+	computeParameters(nPoints, pointsDimension, successProbability, thresholdR, dataSet, availableTotalMemory);
+
+}
 
 void LSH::computeParameters(IntT nPoints, IntT pointsDimension,
 		RealT successProbability, RealT thresholdR, PPointT *dataSetPoints,
 		MemVarT availableTotalMemory) {
+	printf("Start compute parameters...\n");
 
 	IntT nRadii = 1;
 	RealT *listOfRadii = NULL;
@@ -39,6 +74,7 @@ void LSH::computeParameters(IntT nPoints, IntT pointsDimension,
 
 	// Generate a sample query set.
 	// Choose several data set points for the sample query points.
+	printf("Generate sample queries...\n");
 	for (IntT i = 0; i < nSampleQueries; i++) {
 		sampleQueries[i] = dataSetPoints[genRandomInt(0, nPoints - 1)];
 	}
@@ -52,7 +88,7 @@ void LSH::computeParameters(IntT nPoints, IntT pointsDimension,
 	// sample queries generated
 
 	// Only compute the R-NN DS parameters and output them to stdout.
-	printf("%d\n", nRadii);
+	printf("radii count: %d\n", nRadii);
 	transformMemRatios(nRadii, memRatiosForNNStructs);
 	for (IntT i = 0; i < nRadii; i++) {
 		// which sample queries to use
--- sources/LSH.h
+++ sources/LSH.h
@@ -2,6 +2,8 @@
 #define LSH_INCLUDED
 
 #include <stdio.h>
+#include <vector>
+
 #include "BasicDefinitions.h"
 #include "Geometry.h"
 
@@ -10,6 +12,8 @@ public:
     LSH();
     virtual ~LSH() {};
     
+    void computeParametersForData(const std::vector<std::vector<double> >& data);
+
     void computeParameters(IntT numPoints, int pointsDimension,
     	RealT successProbability, RealT thresholdR, PPointT *dataSetPoints,
     	MemVarT availableTotalMemory);
